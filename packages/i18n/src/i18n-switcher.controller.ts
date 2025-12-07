33f8272bd88ab84659f3556d35551d4f47df166e
Makhloufhleli <makhlouf.hleli@gmail.com>
2025-12-07 16:56:05 +0100

    chore: update version to 0.1.3 and refactor i18n service and controller

--- packages/i18n/src/i18n-switcher.controller.ts
+++ packages/i18n/src/i18n-switcher.controller.ts
@@ -16,48 +16,25 @@ export class I18nSwitcherController {
   @Post("switch-locale")
   switchLocale(
     @Body("locale") locale: string,
+    @Body("redirect") redirectPath: string | undefined,
     @Req() req: any,
     @Res() res: any,
   ) {
     if (!locale) {
       return res
         .status(HttpStatus.BAD_REQUEST)
-        .send({ error: "Locale is required" });
+        .send({ success: false, error: "Locale is required" });
     }
 
     const validatedLocale = this.i18nHelper.validateLocale(locale);
     this.i18nHelper.setLocaleCookie(res, validatedLocale);
 
-    const referer = req.headers.referer || req.headers.origin || "/";
-    let currentPath = "/";
-    let query: Record<string, string> = {};
-
-    try {
-      const refererUrl = new URL(referer);
-      const requestOrigin = `${req.protocol}://${req.hostname}${req.hostname === "localhost" && req.port ? ":" + req.port : ""}`;
-
-      if (refererUrl.origin !== requestOrigin) {
-        currentPath = "/";
-      } else {
-        currentPath = refererUrl.pathname;
-        refererUrl.searchParams.forEach((value, key) => {
-          query[key] = value;
-        });
-      }
-    } catch {
-      currentPath = "/";
-    }
-
-    const redirectUrl = this.i18nHelper.buildLocaleUrl(
-      validatedLocale,
-      currentPath,
-      query,
-    );
-
+    console.log('[I18nSwitcher] Locale switched to:', validatedLocale);
+    
+    // Return success - client will handle reload
     return res.send({
       success: true,
       locale: validatedLocale,
-      redirectUrl,
     });
   }
 
